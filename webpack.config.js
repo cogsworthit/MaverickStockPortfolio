725431e66ed7d14ab8cc39b25f2e22a25a1f339e
Jay <hyunjae9034@gmail.com>
2018-09-22 17:13:49 -0400

    refactored process.env

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,36 @@
 var path = require('path');
+var webpack = require('webpack');
+var config = require('dotenv').config();
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    app: ['babel-polyfill', `${SRC_DIR}/index.jsx`]
-  },
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
-  }, 
-  module: {
-    rules: [
-      {
-        test: [/\.js$/, /\.jsx?$/],
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: { 
-          presets: ['env', 'react', 'stage-0']
+let env = config.parsed;
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      app: ['babel-polyfill', `${SRC_DIR}/index.jsx`]
+    },
+    output: {
+      filename: 'bundle.js',
+      path: DIST_DIR
+    }, 
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(env)
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: [/\.js$/, /\.jsx?$/],
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          options: { 
+            presets: ['env', 'react', 'stage-0']
+          }
         }
-      }
-    ]
+      ]
+    }
   }
 };
